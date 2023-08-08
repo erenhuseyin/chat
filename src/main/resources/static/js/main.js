7fb063f4b1c6ae13d581ed02ff9a7a25eb1dbf45
huseyineren <huseyin.eren@finastech.com>
2023-08-08 12:31:20 +0300

    update js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -40,15 +40,6 @@
      connectingElement.classList.add('hidden')
  }
 
- function onError(){
-     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh the page and try';
-     connectingElement.style.color = 'red';
- }
-
- function onMessageReceived(){
-
- }
-
  function sendMessage() {
 
      var messageContent = messageInput.value.trim();
@@ -68,5 +59,56 @@
      event.preventDefault();
  }
 
+ function onError(){
+     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh the page and try';
+     connectingElement.style.color = 'red';
+ }
+
+ function onMessageReceived(payload){
+     var message = JSON.parse(payload.body);
+
+     var messageElement = document.createElement('li');
+
+     if(message.type === 'JOIN') {
+         messageElement.classList.add('event-message');
+         message.content = message.sender + ' joined!';
+     } else if (message.type === 'LEAVE') {
+         messageElement.classList.add('event-message');
+         message.content = message.sender + ' left!';
+     } else {
+         messageElement.classList.add('chat-message');
+
+         var avatarElement = document.createElement('i');
+         var avatarText = document.createTextNode(message.sender[0]);
+         avatarElement.appendChild(avatarText);
+         avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+         messageElement.appendChild(avatarElement);
+
+         var usernameElement = document.createElement('span');
+         var usernameText = document.createTextNode(message.sender);
+         usernameElement.appendChild(usernameText);
+         messageElement.appendChild(usernameElement);
+     }
+
+     var textElement = document.createElement('p');
+     var messageText = document.createTextNode(message.content);
+     textElement.appendChild(messageText);
+
+     messageElement.appendChild(textElement);
+
+     messageArea.appendChild(messageElement);
+     messageArea.scrollTop = messageArea.scrollHeight;
+ }
+
+ function getAvatarColor(messageSender) {
+     var hash = 0;
+     for (var i = 0; i < messageSender.length; i++) {
+         hash = 31 * hash + messageSender.charCodeAt(i);
+     }
+     var index = Math.abs(hash % colors.length);
+     return colors[index];
+ }
+
  usernameForm.addEventListener('submit', connect, true)
- usernameForm.addEventListener('submit', sendMessage, true)
+ messageForm.addEventListener('submit', sendMessage, true)
